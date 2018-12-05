5ff88032e0aa8cfd22db40305a040ce7f2b2918c
Lanxofmine <1025822313.com>
2018-12-05 21:26:08 +0800

    hia

--- HIA/HIA/Form2.Designer.cs
+++ HIA/HIA/Form2.Designer.cs
@@ -28,19 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "请问你是煞笔吗？";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(186, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "是";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseEnter += new System.EventHandler(this.Button1_MouseEnter);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(281, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "不是";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseEnter += new System.EventHandler(this.Button2_MouseEnter);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(368, 200);
+            this.ClientSize = new System.Drawing.Size(368, 130);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
             this.Name = "Form2";
             this.Text = "诶嘿嘿";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form2_FormClosed);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- HIA/HIA/Form2.cs
+++ HIA/HIA/Form2.cs
@@ -1,20 +1,88 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace HIA
 {
     public partial class Form2 : Form
     {
+        Point point;
         public Form2()
         {
             InitializeComponent();
+            foreach (Control item in this.Controls)
+            {
+                if(item is Button)
+                {
+                    item.Click += new EventHandler(Lick);
+                }
+            }
         }
+        private void Lick(object sendr,EventArgs e)
+        {
+            MessageBox.Show("我就知道你会点是的！");
+        }
+        
+        private void Button2_MouseEnter(object sender, EventArgs e)
+        {
+            if (button2.Text.Equals("不是"))
+            {
+                point = new Point();
+                point = button2.Location;
+                if (point.Y > 40)
+                    button2.Location = new Point(point.X, point.Y - 40);
+                else
+                {
+                        button2.Location = new Point(point.X, point.Y + 80);
+                        button2.Text = "是";
+                        button1.Text = "不是";
+                        button2.MouseEnter -= new EventHandler(Button2_MouseEnter);
+                        button2.MouseEnter += new EventHandler(Button2Enter);
+                }
+            }
+
+        }
+
+        private void Button1_MouseEnter(object sender, EventArgs e)
+        {
+            if (button1.Text.Equals("不是"))
+            {
+                point = new Point();
+                point = new Point(button1.Location.X, button1.Location.Y);
+                if (button1.Location.Y > 40)
+                    button1.Location = new Point(point.X, point.Y - 40);
+                else
+                {
+                    button1.Location = new Point(point.X, point.Y + 80);
+                    button1.Text = "是";
+                    button2.Text = "不是";
+                    button1.MouseEnter -= new EventHandler(Button1_MouseEnter);
+                    button1.MouseEnter += new EventHandler(Button1Enter);
+                }
+            }
+        }
+        public void Button1Enter(object sender, EventArgs e)
+        {
+            if (button1.Text.Equals("不是"))
+            {
+                button1.Text = "是";
+                button2.Text = "不是";
+                button1.MouseEnter += new EventHandler(Button1_MouseEnter);
+            }
+        }
+        public void Button2Enter(object sender, EventArgs e)
+        {
+            if (button2.Text.Equals("不是"))
+            {
+                button2.Text = "是";
+                button1.Text = "不是";
+                button2.MouseEnter += new EventHandler(Button2_MouseEnter);
+            }
+        }
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MessageBox.Show("关了窗体也不能否认你是煞笔的事实！");
+        }
+
     }
 }
